77e2cc193ffe24fa85d0762b21c27f2bbefbfe3d
Dawid Sokol <dawidsokol@CF1.local>
2019-05-14 08:12:43 +0100

    w ccp jest 1 program, w consoleApp3 jest 2(w c#)

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Xml;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = new DirectoryInfo(@"C:\Users\dawidsokol\Desktop\gxl");
+            FileInfo[] Files = directory.GetFiles("*.xml");
+            string str = "";
+            int hzz = 0;
+
+            try
+            {
+                foreach (var file in directory.GetFiles())
+                {
+                    str = file.Name;
+                    string path = (@"C:\Users\dawidsokol\Desktop\gxl\" + str);
+
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(path);
+                    XmlNodeList counter = doc.GetElementsByTagName("counter");
+                    XmlNodeList id = doc.GetElementsByTagName("id");
+                    XmlNodeList x = doc.GetElementsByTagName("x");
+                    XmlNodeList y = doc.GetElementsByTagName("y");
+                    XmlNodeList hz = doc.GetElementsByTagName("hz");
+                    XmlNodeList connid = doc.GetElementsByTagName("connid");
+                    XmlNodeList connx = doc.GetElementsByTagName("connx");
+                    XmlNodeList conny = doc.GetElementsByTagName("conny");
+                    XmlNodeList connhz = doc.GetElementsByTagName("connhz");
+                    int[,] tab = new int[counter.Count, 8];
+                    int[,] tab2 = new int[id.Count, 4];
+                    Console.WriteLine("id   X     Y    hz   connid  connX  connY  connhz");
+                    int tmp;
+                    for (int i = 0; i < counter.Count; i++)
+                    {
+                        tmp = Int32.Parse(id[i].InnerXml);
+                        tab[i, 0] = tmp;
+                        tmp = Int32.Parse(x[i].InnerXml);
+                        tab[i, 1] = tmp;
+                        tmp = Int32.Parse(y[i].InnerXml);
+                        tab[i, 2] = tmp;
+                        tmp = Int32.Parse(hz[i].InnerXml);
+                        tab[i, 3] = tmp;
+                        tmp = Int32.Parse(connid[i].InnerXml);
+                        tab[i, 4] = tmp;
+                        tmp = Int32.Parse(connx[i].InnerXml);
+                        tab[i, 5] = tmp;
+                        tmp = Int32.Parse(conny[i].InnerXml);
+                        tab[i, 6] = tmp;
+                        tmp = Int32.Parse(connhz[i].InnerXml);
+                        tab[i, 7] = tmp;
+                    }
+                    tmp = 0;
+                    //for(int h = 0;h<10;h++)
+                    for(int i = 0; i < counter.Count; i ++)
+                    {
+                        if(tab[i,0] == i && tab[i,3] == tab[i,7])
+                        {
+                            tab[i, 3]++;
+                        }
+                        else
+                        {
+                            tmp++;
+                        }
+                    }
+
+                        //d = Math.Sqrt((tab[i, 1] - tab[i, 4]) * (tab[i, 1] - tab[i, 4]) + (tab[i, 2] - tab[i, 5]) * (tab[i, 1] - tab[i, 5]));
+                    //pokazywanie tablicy
+                    for (int i = 0; i < counter.Count; i++)
+                    {
+                        for (int j = 0; j < 8; j++)
+                        {
+                            Console.Write(tab[i, j] + "     ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("File not exists");
+            }
+
+        }
+    }
+}
---
+++ cpp/cpp/cpp.cpp
@@ -0,0 +1,148 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+//usuwamy plik xml przed nastepnym uzyciem programu
+int main()
+{
+	int R = 10;
+	int r = 2, counter = 0;
+	int k = 0;
+	int hz = 0;
+	int N = 10, i = 0;
+	int tab[10][10];		//stala wartosc N
+	int y, x, count = 0;
+	float d, dd;
+	srand(time(NULL));
+	fstream file;
+	file.open("C:\\Users\\dawidsokol\\Desktop\\gxl\\gxlfile.xml", ios::app); //app::ios
+	file << "<?xml version='1.0' encoding='utf-8'?>" << endl;
+	file << "<node>" << endl;
+	while (i < N)
+	{
+		x = (((float)rand() / RAND_MAX) * (R * 2)) + ((R) * (-1));
+		y = (((float)rand() / RAND_MAX) * (R * 2)) + ((R) * (-1));
+		d = sqrt((x - 0) * (x - 0) + (y - 0) * (y - 0));
+		if (d <= R)
+		{
+			i++;
+			tab[k][0] = x;
+			tab[k][1] = y;
+			tab[k][2] = hz;
+			tab[k][3] = counter;
+			counter++;
+			//cout<<"tab[0] "<<tab[k][0]<<"     tab[1] "<<tab[k][1]<<"      tab[2] "<<tab[k][2]<<endl;
+			k++;
+		}
+	}
+	k = 0;
+	//for(int s=0;s<(N*N);s++)
+	while (k < N)
+	{
+		int tmp = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if (k <= 0)
+			{
+				tmp++;
+				dd = sqrt((tab[k][0] - tab[tmp][0]) * (tab[k][0] - tab[tmp][0]) + (tab[k][1] - tab[tmp][1]) * (tab[k][1] - tab[tmp][1]));
+				if (dd <= (2 * r) && tab[k][2] == tab[tmp][2])
+				{
+					if (tab[k][3] == tab[tmp][3])
+					{
+					}
+					else
+					{
+						file << "<edge>" << endl;
+						file << "<counter>" << count << "</counter>" << endl;
+						file << "<id>" << tab[k][3] << "</id>" << endl;
+						file << "<x>" << tab[k][0] << "</x>" << endl;
+						file << "<y>" << tab[k][1] << "</y>" << endl;
+						file << "<hz>" << tab[k][2] << "</hz>" << endl;
+						file << "<connid>" << tab[tmp][3] << "</connid>" << endl;
+						file << "<connx>" << tab[tmp][0] << "</connx>" << endl;
+						file << "<conny>" << tab[tmp][1] << "</conny>" << endl;
+						file << "<connhz>" << tab[tmp][2] << "</connhz>" << endl;
+						file << "</edge>" << endl;
+						cout << "<edge from=" << tab[k][3] << " to=" << tab[tmp][3] << " ></edge>" << endl;
+						count++;
+					}
+				}
+			}
+			else
+			{
+				if (tmp < k)
+				{
+					dd = sqrt((tab[k][0] - tab[tmp][0]) * (tab[k][0] - tab[tmp][0]) + (tab[k][1] - tab[tmp][1]) * (tab[k][1] - tab[tmp][1]));
+					tmp++;
+					if (dd <= (2 * r) && tab[k][2] == tab[tmp][2])
+					{
+						if (tab[k][3] == tab[tmp][3])
+						{
+						}
+						else
+						{
+							file << "<edge>" << endl;
+							file << "<counter>" << count << "</counter>" << endl;
+							file << "<id>" << tab[k][3] << "</id>" << endl;
+							file << "<x>" << tab[k][0] << "</x>" << endl;
+							file << "<y>" << tab[k][1] << "</y>" << endl;
+							file << "<hz>" << tab[k][2] << "</hz>" << endl;
+							file << "<connid>" << tab[tmp][3] << "</connid>" << endl;
+							file << "<connx>" << tab[tmp][0] << "</connx>" << endl;
+							file << "<conny>" << tab[tmp][1] << "</conny>" << endl;
+							file << "<connhz>" << tab[tmp][2] << "</connhz>" << endl;
+							file << "</edge>" << endl;
+							cout << "<edge from=" << tab[k][3] << " to=" << tab[tmp][3] << " ></edge>" << endl;
+							count++;
+						}
+					}
+				}
+				else if (tmp == k)
+				{
+					tmp++;
+				}
+				else if (tmp > k)
+				{
+					dd = sqrt((tab[k][0] - tab[tmp][0]) * (tab[k][0] - tab[tmp][0]) + (tab[k][1] - tab[tmp][1]) * (tab[k][1] - tab[tmp][1]));
+					if (dd <= (2 * r) && tab[k][2] == tab[tmp][2])
+					{
+						if (tab[k][3] == tab[tmp][3])
+						{
+						}
+						else
+						{
+							file << "<edge>" << endl;
+							file << "<counter>" << count << "</counter>" << endl;
+							file << "<id>" << tab[k][3] << "</id>" << endl;
+							file << "<x>" << tab[k][0] << "</x>" << endl;
+							file << "<y>" << tab[k][1] << "</y>" << endl;
+							file << "<hz>" << tab[k][2] << "</hz>" << endl;
+							file << "<connid>" << tab[tmp][3] << "</connid>" << endl;
+							file << "<connx>" << tab[tmp][0] << "</connx>" << endl;
+							file << "<conny>" << tab[tmp][1] << "</conny>" << endl;
+							file << "<connhz>" << tab[tmp][2] << "</connhz>" << endl;
+							file << "</edge>" << endl;
+							cout << "<edge from=" << tab[k][3] << " to=" << tab[tmp][3] << " ></edge>" << endl;
+							count++;
+						}
+					}
+					tmp++;
+				}
+			}
+		}
+		//cout<<"X: "<< x << " Y: "<< y <<" Hz: "<<hz<< endl;
+		k++;
+	}
+
+	for(int k =0 ;k < N;k++)
+	{
+	    cout<<"Pkt: "<<k<<"   X: "<<tab[k][0]<<"     Y: "<<tab[k][1]<<"      Hz: "<<tab[k][2]<<endl;
+	}
+	file << "</node>" << endl;
+	file.close();
+	return 0;
+}
